a26e5170cbbe05d0dce27f9fb78b3380c539af7a
Hp <lolitakur72@gmail.com>
2021-07-03 12:15:24 +0500

    1

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Students(models.Model):
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+import random
+from mainapp.models import Students
 
-# Create your views here.
+def index(request):
+    name = ['Ложкина', "Обухов", "Достовалов", "Карпор", "Зырянов", "Пухов", "Теплов", "Газиев", "Курбонова", "Велижанина"]
+    random_name = random.choice(name)
+    context = {
+        'name': random_name,
+        'title':'Главная'
+    }
+    return render(request, 'mainapp/index.html', context)
+
+
+
+def student(request):
+    students = Students.objects.all()
+    context = {
+        'students': students,
+        'page_title': 'список'
+    }
+    return render(request, 'mainapp/student.html', context)
