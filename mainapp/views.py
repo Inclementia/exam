5045d34516f2e3e560e5e3d29f097fc37b6a6c6f
Hp <lolitakur72@gmail.com>
2021-07-03 13:03:07 +0500

    1

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 import random
 from mainapp.models import Students
 
+
 def index(request):
-    name = ['Ложкина', "Обухов", "Достовалов", "Карпор", "Зырянов", "Пухов", "Теплов", "Газиев", "Курбонова", "Велижанина"]
+    name = ['Ложкина', "Обухов", "Достовалов", "Карпор", "Зырянов", "Пухов", "Теплов", "Газиев", "Курбонова",
+            "Велижанина"]
     random_name = random.choice(name)
     context = {
         'name': random_name,
-        'title':'Главная'
+        'title': 'Главная'
     }
     return render(request, 'mainapp/index.html', context)
-
 
 
 def student(request):
